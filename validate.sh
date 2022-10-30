7826d0e82e68f4d9f66d97969a799a43c79ee223
alexmalder <vnmntn@mail.ru>
2022-10-30 14:38:24 +0300

    validate program variable

--- validate.sh
+++ validate.sh
@@ -2,13 +2,15 @@
 
 set -eo pipefail
 
+diff_prog="colordiff"
+
 diff_not_origin() {
     find_path=$1
     sed_pattern=$2
     for file in $(find ${find_path} -type f); do
         sed_file=$(echo "$file" | sed "s/$sed_pattern//g")
-        echo "colordiff $file $HOME/$sed_file"
-        colordiff $file $HOME/$sed_file
+        echo "$diff_prog $file $HOME/$sed_file"
+        $diff_prog $file $HOME/$sed_file
     done
 }
 
@@ -16,14 +18,14 @@ diff_by_path() {
     find_path=$1
     second_path=$2
     for file in $(find ${find_path} -type f); do
-        echo "colordiff $file $second_path/$file"
-        colordiff $file $second_path/$file
+        echo "$diff_prog $file $second_path/$file"
+        $diff_prog $file $second_path/$file
     done
 }
 
 main() {
     diff_by_path .config $HOME
-    diff_not_origin home "home/"
+    diff_not_origin home "home\/"
     diff_by_path etc
 }
 
