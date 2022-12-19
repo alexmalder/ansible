55c65cd8e9ab2f576a1321500fb34777f8d37853
alexmalder <vnmntn@mail.ru>
2022-12-19 20:29:41 +0300

    tasks in playbooks removed

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 upgrade_cluster:
 	ansible-playbook haproxy.yml
-	ansible-playbook sshd.yml
 	ansible-playbook debug-ip-addr.yml
+	ansible-playbook sshd.yml
+	ansible-playbook bind.yml
--- scripts/git-wrapper.sh
+++ scripts/git-wrapper.sh
@@ -11,4 +11,5 @@ for repository in $(ls $WORKDIR); do
     echo "$WORKDIR/$repository"
     git -C $WORKDIR/$repository remote add gitlab $GITLAB_USER@$GITLAB_DOMAIN:$GITLAB_GROUP/$repository
     git -C $WORKDIR/$repository push -u gitlab master
+    git -C $WORKDIR/$repository push -u origin master
 done
