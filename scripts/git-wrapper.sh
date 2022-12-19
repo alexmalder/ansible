b9265e53593ad1be14f6096591cf9de3b5f54afc
alexmalder <vnmntn@mail.ru>
2022-12-19 20:35:05 +0300

    wrapper modified

--- scripts/git-wrapper.sh
+++ scripts/git-wrapper.sh
@@ -6,10 +6,12 @@ WORKDIR=$HOME/Git
 GITLAB_USER=git
 GITLAB_DOMAIN=gitlab.com
 GITLAB_GROUP=alex_malder
+remotes=("gitlab" "origin") # origin is github
 
 for repository in $(ls $WORKDIR); do
     echo "$WORKDIR/$repository"
     git -C $WORKDIR/$repository remote add gitlab $GITLAB_USER@$GITLAB_DOMAIN:$GITLAB_GROUP/$repository
-    git -C $WORKDIR/$repository push -u gitlab master
-    git -C $WORKDIR/$repository push -u origin master
+    for remote in $remotes; do
+        git -C $WORKDIR/$repository push -u $remote master
+    done
 done
