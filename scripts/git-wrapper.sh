5be64028a0161fde1474762055cb3cd52b0ed4cf
alexmalder <vnmntn@mail.ru>
2022-12-21 01:35:20 +0300

    cluster network

--- scripts/git-wrapper.sh
+++ scripts/git-wrapper.sh
@@ -3,19 +3,18 @@
 #set -eo pipefail
 
 WORKDIR=$HOME/Git
-GITLAB_USER=git
-GITLAB_DOMAIN=gitlab.com
-GITLAB_GROUP=alex_malder
-remotes=("gitlab" "origin") # origin is github
+NEW_REMOTE_DOMAIN=http://git.vnmntn.com
+NEW_REMOTE_GROUP=alexmalder
+remotes=("gitea" "origin") # origin is github
 
 for repository in $(ls $WORKDIR); do
-    #git -C $WORKDIR/$repository remote add gitlab $GITLAB_USER@$GITLAB_DOMAIN:$GITLAB_GROUP/$repository > /dev/null 2>&1
+    git -C $WORKDIR/$repository remote add gitea $NEW_REMOTE_DOMAIN/$NEW_REMOTE_GROUP/$repository > /dev/null 2>&1
     #echo ""
     #echo "REPOSITORY: [ $repository ]  "
     #echo ""
     for remote in "${remotes[@]}"; do
         #echo "git -C $repository push -u $remote master"
-        git -C $WORKDIR/$repository push -u $remote master > /dev/null 2>&1
+        git -C $WORKDIR/$repository push -u $remote master #> /dev/null 2>&1
     done
     git -C $WORKDIR/$repository log -1 --oneline
 done
