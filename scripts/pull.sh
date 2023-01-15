a2e6f96dd19c168d4f59f7d100174f8d500a56cf
alexmalder <vnmntn@mail.ru>
2023-01-16 01:48:24 +0300

    fix lint

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -10,3 +10,4 @@ cp -rv $XDG_CONFIG_HOME/nvim $WORKDIR/.config
 cp -rv $XDG_CONFIG_HOME/kitty $WORKDIR/.config
 cp -rv $XDG_CONFIG_HOME/qutebrowser $WORKDIR/.config
 cp -rv $XDG_CONFIG_HOME/picom.conf $WORKDIR/.config
+cp -rv $XDG_CONFIG_HOME/helm $WORKDIR/.config
