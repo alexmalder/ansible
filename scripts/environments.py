1e4aa0ff62cde37fdb28e3fb4c301bebe72f2d62
Alex Malder <vnmntn@mail.ru>
2023-02-05 20:56:35 +0300

    env script only print

--- scripts/environments.py
+++ scripts/environments.py
@@ -13,11 +13,16 @@ def iter_projects(gitlab_url, gl):
         web_url = web_url.split(gitlab_url+"/")[1]
         item["name"] = web_url
         item["type"] = "project"
+        print("- project: %s" % (web_url))
         variables = repository.variables.list(get_all=True)
         if len(variables) > 0:
             item["variables"] = []
             for variable in variables:
                 variable = variable.asdict()
+                if variable["variable_type"] == "file":
+                    print("  %s: |\n%s" % (variable["key"], variable["value"]))
+                else:
+                    print("  %s: %s" % (variable["key"], variable["value"]))
                 item["variables"].append({variable["key"]: variable["value"]})
             items.append(item)
     return items
@@ -31,6 +36,7 @@ def iter_groups(gitlab_url, gl):
         group_dict = group.asdict()
         web_url = group_dict["web_url"]
         web_url = web_url.split(gitlab_url+"/groups/")[1]
+        print("- group: %s" % (web_url))
         item["name"] = web_url
         item["type"] = "group"
         variables = group.variables.list()
@@ -38,6 +44,11 @@ def iter_groups(gitlab_url, gl):
             item["variables"] = []
             for variable in variables:
                 variable = variable.asdict()
+                if variable["variable_type"] == "file":
+                    print("  %s: |\n%s" % (variable["key"], variable["value"]))
+                else:
+                    print("  %s: %s" % (variable["key"], variable["value"]))
+                item["variables"].append({variable["key"]: variable["value"]})
                 item["variables"].append({variable["key"]: variable["value"]})
             items.append(item)
     return items
@@ -49,13 +60,6 @@ def main():
     gl = gitlab.Gitlab("https://" + gitlab_url, gitlab_token)
     group_variables = iter_groups(gitlab_url, gl)
     project_variables = iter_projects(gitlab_url, gl)
-    with open("variables.yaml", "w") as f:
-        yaml.dump(
-            group_variables + project_variables,
-            f,
-            sort_keys=False,
-            default_flow_style=False,
-        )
 
 
 main()
