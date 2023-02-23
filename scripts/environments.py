8480d38f347a906979babd676f20d2cf8e7f965e
Alex Malder <vnmntn@mail.ru>
2023-02-23 23:43:38 +0300

    gitlab environment works with base64

--- scripts/environments.py
+++ scripts/environments.py
@@ -1,6 +1,7 @@
 import os
 import gitlab
 import yaml
+import base64
 
 
 def iter_projects(gitlab_url, gl):
@@ -10,20 +11,20 @@ def iter_projects(gitlab_url, gl):
         item = {}
         repo = repository.asdict()
         web_url = repo["web_url"]
-        web_url = web_url.split(gitlab_url+"/")[1]
+        web_url = web_url.split(gitlab_url + "/")[1]
         item["name"] = web_url
         item["type"] = "project"
-        print("- project: %s" % (web_url))
         variables = repository.variables.list(get_all=True)
         if len(variables) > 0:
             item["variables"] = []
             for variable in variables:
                 variable = variable.asdict()
-                if variable["variable_type"] == "file":
-                    print("  %s: |\n%s" % (variable["key"], variable["value"]))
-                else:
-                    print("  %s: %s" % (variable["key"], variable["value"]))
-                item["variables"].append({variable["key"]: variable["value"]})
+                if variable["variable_type"] == "env_var":
+                    item["variables"].append({variable["key"]: variable["value"]})
+                elif variable["variable_type"] == "file":
+                    byteval = variable["value"].encode("utf-8")
+                    encoded_val = base64.b64encode(byteval).decode("utf-8")
+                    item["variables"].append({variable["key"]: encoded_val})
             items.append(item)
     return items
 
@@ -35,8 +36,7 @@ def iter_groups(gitlab_url, gl):
         item = {}
         group_dict = group.asdict()
         web_url = group_dict["web_url"]
-        web_url = web_url.split(gitlab_url+"/groups/")[1]
-        print("- group: %s" % (web_url))
+        web_url = web_url.split(gitlab_url + "/groups/")[1]
         item["name"] = web_url
         item["type"] = "group"
         variables = group.variables.list()
@@ -44,22 +44,24 @@ def iter_groups(gitlab_url, gl):
             item["variables"] = []
             for variable in variables:
                 variable = variable.asdict()
-                if variable["variable_type"] == "file":
-                    print("  %s: |\n%s" % (variable["key"], variable["value"]))
-                else:
-                    print("  %s: %s" % (variable["key"], variable["value"]))
-                item["variables"].append({variable["key"]: variable["value"]})
-                item["variables"].append({variable["key"]: variable["value"]})
+                if variable["variable_type"] == "env_var":
+                    item["variables"].append({variable["key"]: variable["value"]})
+                elif variable["variable_type"] == "file":
+                    byteval = variable["value"].encode("utf-8")
+                    encoded_val = base64.b64encode(byteval).decode("utf-8")
+                    item["variables"].append({variable["key"]: encoded_val})
+
             items.append(item)
     return items
 
 
 def main():
-    gitlab_url = os.getenv("GIT_URL_SECURE", "")
-    gitlab_token = os.getenv("GIT_PASSWORD_SECURE", "")
+    gitlab_url = os.getenv("GIT_URL_PUBLIC", "")
+    gitlab_token = os.getenv("GIT_PASSWORD_PUBLIC", "")
     gl = gitlab.Gitlab("https://" + gitlab_url, gitlab_token)
     group_variables = iter_groups(gitlab_url, gl)
     project_variables = iter_projects(gitlab_url, gl)
+    print(yaml.safe_dump({"groups": group_variables, "projects": project_variables}))
 
 
 main()
