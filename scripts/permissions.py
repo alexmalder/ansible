31bf75720462ab2c15bdf946909ead2cc82861e2
Alex Malder <vnmntn@mail.ru>
2023-03-26 01:00:29 +0300

    simple perms

--- scripts/permissions.py
+++ scripts/permissions.py
@@ -1,31 +1,19 @@
 import os
 import gitlab
-import yaml
 
 gitlab_url = os.getenv("GIT_URL_SECURE", "")
 gitlab_token = os.getenv("GIT_PASSWORD_SECURE", "")
 
-items = []
+def iter_item(items):
+    for project in items:
+        members_by_project = project.members.list(get_all=True)
+        project_dict = project.asdict()
+        print(project_dict["web_url"]+":")
+        for membership in members_by_project:
+            memberdict = membership.asdict()
+            print("  - "+memberdict["username"] + ":" + str(memberdict["access_level"]))
+
+
 with gitlab.Gitlab("https://" + gitlab_url, gitlab_token) as gl:
-    users = gl.users.list()
-    for user in users:
-        dictionary = {}
-        memberships = user.memberships.list(get_all=True)
-        user_dict = user.asdict()
-        dictionary["username"] = user_dict["username"]
-        dictionary["services"] = []
-        for membership in memberships:
-            member_dict = membership.asdict()
-            dictionary["services"].append(
-                {
-                    "source_name": member_dict["source_name"],
-                    "access_level": member_dict["access_level"],
-                    "source_type": member_dict["source_type"],
-                }
-            )
-        items.append(dictionary)
-
-print(items)
-
-with open('permissions.yaml', 'w') as f:
-    yaml.dump(items, f, sort_keys=False, default_flow_style=False)
+    iter_item(gl.projects.list(get_all=True, owned=True))
+    iter_item(gl.groups.list(get_all=True, owned=True))
