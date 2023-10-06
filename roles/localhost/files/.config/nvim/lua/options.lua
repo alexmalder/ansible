1048bb0296fc79d5f49625008187ea79902d71a7
udalov_av <udalov_av@magnit.ru>
2023-10-07 01:47:07 +0300

    force restore commit history

--- roles/localhost/files/.config/nvim/lua/options.lua
+++ roles/localhost/files/.config/nvim/lua/options.lua
@@ -73,6 +73,10 @@ neogit.setup()
 -- Lua initialization file
 --vim.cmd [[colorscheme vscode]]
 
+vim.keymap.set('n', '<tab>', function() return require('fold-cycle').open() end, {silent = true, desc = 'Fold-cycle: open folds'})
+vim.keymap.set('n', '<s-tab>', function() return require('fold-cycle').close() end, {silent = true, desc = 'Fold-cycle: close folds'})
+vim.keymap.set('n', 'zC', function() return require('fold-cycle').close_all() end, {remap = true, silent = true, desc = 'Fold-cycle: close all folds'})
+
 
 require("transparent").setup({
   groups = { -- table: default groups
