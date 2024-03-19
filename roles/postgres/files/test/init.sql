784afdbad0157727dfc6fe119940cb40792b4128
alexmalder <95620403+alexmalder@users.noreply.github.com>
2024-03-20 00:59:51 +0300

    Postgres minimal (#31)
    * pg minimal
    
    * lesson added
    
    * postgres readme updated
    
    * dua cli package added

--- roles/postgres/files/test/init.sql
+++ roles/postgres/files/test/init.sql
@@ -1,3 +1,4 @@
+-- удаляем таблицы, чтобы далее создать их с нуля
 DROP TABLE IF EXISTS students CASCADE;
 DROP TABLE IF EXISTS courses CASCADE;
 DROP TABLE IF EXISTS courses_students CASCADE;
@@ -27,11 +28,6 @@ CREATE TABLE IF NOT EXISTS polls (
   course_id INTEGER REFERENCES courses(id)
 );
 
-CREATE TABLE IF NOT EXISTS instances (
-  id SERIAL PRIMARY KEY,
-  poll_id INTEGER REFERENCES polls(id)
-);
-
 CREATE TABLE IF NOT EXISTS questions (
   id SERIAL PRIMARY KEY,
   title VARCHAR(128) NOT NULL,
@@ -43,114 +39,44 @@ CREATE TABLE IF NOT EXISTS answers (
   title VARCHAR(128) NOT NULL,
   choise INTEGER NOT NULL,
   student_id INTEGER REFERENCES students(id),
-  instance_id INTEGER REFERENCES instances(id),
+  polls_id INTEGER REFERENCES pollss(id),
   question_id INTEGER REFERENCES questions(id),
-  PRIMARY KEY (student_id, instance_id, question_id)
+  PRIMARY KEY (student_id, polls_id, question_id)
 );
+
+-- 1. Создать студента
+-- 2. Создать курсы
+-- 3. Привязать курсы к студенту
+-- 4. Создать новый опрос
+-- 5. Привязать к опросу вопросы и ответы
+-- 6. Пройти свой же опрос для теста
 
 INSERT INTO students(name) VALUES ('dmitry'), ('sergey'), ('alex');
 INSERT INTO courses(name) VALUES ('devops'), ('cv'), ('nlp');
 INSERT INTO courses_students (student_id,course_id) VALUES (1,1), (1,2), (2,2), (2,3), (3,1);
 
--- Create or replace o_students_with_courses type
-DO $$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'o_students_with_courses') THEN
-      CREATE TYPE o_students_with_courses AS
-      (
-        id INTEGER,
-        name VARCHAR(128),
-        cnames text[]
-      );
-  END IF;
-END$$;
-
--- Create or replace o_courses type
-DO $$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'o_courses') THEN
-      CREATE TYPE o_courses AS
-      (
-        id INTEGER,
-        name VARCHAR(128)
-      );
-  END IF;
-END$$;
-
--- function select_courses_lf (limit, offset)
-CREATE OR REPLACE FUNCTION select_courses_lf(INTEGER, INTEGER)
-RETURNS o_courses[] LANGUAGE plpgsql AS $$
-DECLARE
-inputLimit ALIAS FOR $1;
-inputOffset ALIAS FOR $2;
-outCourses o_courses[];
-BEGIN
-  select into outCourses courses.* from courses limit inputLimit offset inputOffset;
-  --select * from courses into outCourses;
-  RETURN outCourses;
-END;
-$$;
-
--- function insert_course (name)
-CREATE OR REPLACE FUNCTION insert_course(text)
-RETURNS INTEGER LANGUAGE plpgsql AS $$
-DECLARE
-inputName ALIAS FOR $1;
-outID INTEGER;
-BEGIN
-  insert into courses (id, name) values (default, inputName) returning * into outID;
-  RETURN outID;
-END;
-$$;
-
--- function update_course (id, name)
-CREATE OR REPLACE FUNCTION update_course(INTEGER, text)
-RETURNS INTEGER LANGUAGE plpgsql AS $$
-DECLARE
-inputID ALIAS FOR $1;
-inputName ALIAS FOR $2;
-outID INTEGER;
-BEGIN
-  update courses set name = inputName where id = inputID returning * into outID;
-  RETURN outID;
-END;
-$$;
-
--- function delete_course (id)
-CREATE OR REPLACE FUNCTION delete_course(INTEGER)
-RETURNS INTEGER LANGUAGE plpgsql AS $$
-DECLARE
-inputID ALIAS FOR $1;
-outID INTEGER;
-BEGIN
-  delete from courses where id = inputID returning * into outID;
-  RETURN outID;
-END;
-$$;
-
 -- function get_student_with_courses_lf (limit, offset, like_course)
-CREATE OR REPLACE FUNCTION get_student_with_courses_lf(INTEGER, INTEGER, text)
-RETURNS o_students_with_courses[] LANGUAGE plpgsql AS $$
-DECLARE
-vLimit ALIAS FOR $1;
-vOffset ALIAS for $2;
-likeCourse ALIAS for $3;
-outStudentsWithCourses o_students_with_courses[];
-BEGIN
-  select into outStudentsWithCourses students.*,
-  --json_agg(json_build_object('id', courses.id, 'name', courses.name)) AS courses
-  ARRAY_AGG (courses.name) as cnames
-  FROM courses
-  LEFT OUTER JOIN courses_students
-  ON courses.id = courses_students.course_id
-  LEFT OUTER JOIN students
-  ON courses_students.student_id = students.id
-  where courses.name like likeCourse
-  GROUP BY students.id
-  limit vLimit
-  offset vOffset;
-  RETURN outStudentsWithCourses;
-END;
-$$;
-
+-- CREATE OR REPLACE FUNCTION get_student_with_courses_lf(INTEGER, INTEGER, text)
+-- RETURNS o_students_with_courses[] LANGUAGE plpgsql AS $$
+-- DECLARE
+-- vLimit ALIAS FOR $1;
+-- vOffset ALIAS for $2;
+-- likeCourse ALIAS for $3;
+-- outStudentsWithCourses o_students_with_courses[];
+-- BEGIN
+--   select into outStudentsWithCourses students.*,
+--   --json_agg(json_build_object('id', courses.id, 'name', courses.name)) AS courses
+--   ARRAY_AGG (courses.name) as cnames
+--   FROM courses
+--   LEFT OUTER JOIN courses_students
+--   ON courses.id = courses_students.course_id
+--   LEFT OUTER JOIN students
+--   ON courses_students.student_id = students.id
+--   where courses.name like likeCourse
+--   GROUP BY students.id
+--   limit vLimit
+--   offset vOffset;
+--   RETURN outStudentsWithCourses;
+-- END;
+-- $$;
 --select get_student_with_courses_lf(128, 0, 'cv');
