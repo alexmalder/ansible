caad906a4afd869a89d9b379dd126b628f37a75d
alexmalder <vnmntn@mail.ru>
2024-05-18 00:18:17 +0300

    sync config files

--- validate.sh
+++ validate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-base_src=roles/localhost/files/.config
+config_base_src=roles/localhost/files/.config
 declare -a config_dirs
 config_dirs[0]="fish/conf.d"
 config_dirs[1]=nvim/lua
 
 for directory in "${config_dirs[@]}"; 
 do
-  files=($(find ${base_src}/${directory} -type f))
+  files=($(find ${config_base_src}/${directory} -type f))
   for file in "${files[@]}";
   do
     clean_path=$(echo $file | sed "s/.*config\///g")
-    stored_path=${base_src}/${clean_path}
+    stored_path=${config_base_src}/${clean_path}
     origin_path="${HOME}/.config/${clean_path}"
     echo "diff --color ${origin_path} ${stored_path}"
     diff --color ${origin_path} ${stored_path}
