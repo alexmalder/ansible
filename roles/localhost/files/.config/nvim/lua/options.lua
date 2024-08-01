1aacd01d8e81d8e62f0bed02b7b7bd35d7e5d0c6
alexmalder <vnmntn@mail.ru>
2024-08-01 03:22:33 +0300

    statuslines

--- roles/localhost/files/.config/nvim/lua/options.lua
+++ roles/localhost/files/.config/nvim/lua/options.lua
@@ -34,34 +34,49 @@ vim.cmd[[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {fo
 -- autopairs
 require'nvim-autopairs'.setup{}
 
--- lsp signature
---cfg = { toggle_key = '<M-r>' }
---require'lsp_signature'.setup{cfg}
-
 -- statusline
-require('wlsample.vscode')
-local windline = require('windline')
-windline.setup({
-    colors_name = function(colors)
-        colors.StatusFg = colors.ActiveFg
-        colors.StatusBg = "#000000"
-        return colors
-    end,
-    statuslines = {
-        default,
-        explorer,
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
     },
-})
-
-local git_comps = require('windline.components.git')
-
-local git_branch = {
-    text = git_comps.git_branch(),
-    hl_colors = {'white','black'},
-    width = 100,
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
 
---require("flutter-tools").setup {}
+
 
 -- git
 require('gitsigns').setup{}
@@ -89,3 +104,6 @@ vim.opt.termguicolors = true
 --     enable = true
 --   },
 -- }
+
+local neogit = require('neogit')
+neogit.setup {}
